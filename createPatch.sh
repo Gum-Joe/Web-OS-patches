57246ecd5cec8ecf834788bd97393b067c1c7e19
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-10-18 13:58:18 +0100

    Add patches

--- createPatch.sh
+++ createPatch.sh
@@ -4,6 +4,8 @@
 mkdir patches
 mkdir patches/web-os
 # Create web-os patches
+echo 'Generating patches for web-os'
+echo
 cd repos/web-os
 COM=`git rev-list HEAD --count`
 git format-patch -$COM -o ../../patches/web-os
