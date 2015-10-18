cb33580a55c8fac4abbe00f63ff86eab75ae8cdf
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-10-18 13:40:31 +0100

    Create a clone.sh

---
+++ clone.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+mkdir repos
+echo Cloneing repos...
+echo
+git clone https://github.com/Gum-Joe/Web-OS-container repos/web-os-container
+echo
+git clone https://github.com/Gum-Joe/Web-OS repos/web-os
+
+mkdir patches
---
+++ generatepatch.sh
@@ -0,0 +1,2 @@
+cd repos/web-os
+git f
