0689e1615fa9ad09a8de3054e7129587cee8d9d2
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-10-18 13:44:35 +0100

    Create a generatePatches script

--- clone.sh
+++ clone.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 mkdir repos
+echo Removing previous repos and patches...
+rm -rf repos
+rm -rf patches
 echo Cloneing repos...
 echo
 git clone https://github.com/Gum-Joe/Web-OS-container repos/web-os-container
 echo
 git clone https://github.com/Gum-Joe/Web-OS repos/web-os
-
-mkdir patches
+exit 0
--- generatepatch.sh
+++ generatepatch.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/bash
+
+# Patches dir
+mkdir patches
+# Create web-os patches
 cd repos/web-os
 git f
